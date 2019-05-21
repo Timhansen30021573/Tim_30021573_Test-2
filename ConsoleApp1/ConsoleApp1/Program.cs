53032f2a3e1364dfb9b2cc0b8d01896075a80c2f
Timothy Hansen <30021573@student.toiohomai.ac.nz>
2019-05-22 10:24:59 +1200

    TimHansen_30021573_Test2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {// Tim Hansen//
+        static void Main(string[] args)//
+        { //Didnt do enough Classes so i put it in something i knew//
+            Console.WriteLine("Your salary is set at 80000 per year");
+            // dont know why its not reading this line//
+            double money = double.Parse(Console.ReadLine());
+            Console.WriteLine("Plesae enter the how many weeks you work");
+            double week = double.Parse(Console.ReadLine());
+            // dont understand why this wont run
+
+
+
+            //This will Write what the total of your salary a week is//
+            Console.WriteLine($"Your salary a week is" + Total(money, week));
+            Console.ReadLine(); 
+        }
+        static double Total(double money, double week) {
+
+
+            //this returns the method
+            return money / week;
+
+        }
+
+    
+
+     
+
+
+
+
+
+
+
+    
+
+  
+
+
+
+
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+            Console.WriteLine("I will calculate your wages");
+            int numHours = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine();
+        }
+    }
+}
